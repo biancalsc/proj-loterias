992b309ab77a529ca57e90e23f9ecc627ceb561b
biancalsc <biancalsc1409@gmail.com>
2023-11-14 16:39:55 -0300

    ativiade 3 pronta

---
+++ src/components/index.ts
@@ -0,0 +1,21 @@
+import Principal from "./Principal";
+import Carregando from "./Carregando";
+import Esquerda from "./Esquerda";
+import Direita from "./Direita";
+import Resultado from "./Resultado";
+import NomeLoteria from "./NomeLoteria";
+import Estimativa from "./Estimativa";
+import Acumulou from "./Acumulou";
+import Menu from "./Menu";
+
+export {
+  Carregando,
+  Principal,
+  Menu,
+  Esquerda,
+  Direita,
+  Resultado,
+  NomeLoteria,
+  Estimativa,
+  Acumulou,
+};
